f7092bde79279cf36fa880c60094e35dfc30307a
Reid <rreid112@gmail.com>
2019-05-13 15:43:11 -0600

    Working

--- database.php
+++ database.php
@@ -1,17 +1,16 @@
 <?php
 
-$server = "enter server";
-$username = "enter username";
-$password = "enter password";
+$server = "server";
+$username = "username";
+$password = "password";
 
 // Create connection
-$conn = mysql_connect($server, $username, $password);
+$conn = mysql_connect($server, $username, $password, $database);
+mysql_select_db('api');
 
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysql_connect_error());
 }
-
-echo "Connected successfully";
 
 ?>

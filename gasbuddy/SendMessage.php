7116803d8e5a257dc2641b4dc4d59b059d30faba
Reid <rreid112@gmail.com>
2019-05-13 14:47:14 -0600

    Adding script to send a message

---
+++ gasbuddy/SendMessage.php
@@ -0,0 +1,28 @@
+<?php
+
+// Include database.php
+include_once('database.php');
+
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+    // Get data
+    $name = isset($_POST['name']) ? mysql_real_escape_string($_POST['name']) : "Anonymous";
+    $message = isset($_POST['message']) ? mysql_real_escape_string($_POST['message']) : "";
+
+    // Insert data into data base
+    $sql = "INSERT INTO `gb_messages` (`id`, `name`, `message`, `shown`) VALUES (NULL, '$name', '$message', '0');";
+    $query = mysql_query($sql);
+
+    if($query) {
+        $json = array("status" => 1, "msg" => "Success!");
+    } else {
+        $json = array("status" => 0, "msg" => "Error!");
+    }
+} else {
+    $json = array("status" => 0, "msg" => "Request method not accepted");
+}
+
+@mysql_close($conn);
+
+/* Output header */
+header('Content-type: application/json');
+echo json_encode($json);
